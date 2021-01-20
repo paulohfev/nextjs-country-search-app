a4111d9b4e5dd1e8dc7104f2c7b189e2ee0e8cc8
paulohfev <phfernandesev@gmail.com>
2021-01-20 10:32:02 -0300

    Add SearchBar component

---
+++ pages/countries/[id].jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+
+const Page = () => {
+  const router = useRouter();
+  const { id } = router.query;
+
+  return (
+    <div>Country Page {id}</div>
+  )
+}
+
+export default Page;
---
+++ src/components/SearchBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SearchBar = () => {
+  return (
+    <div>
+      <label>Type out your country's name</label>
+      <input type="text" />
+    </div>
+  )
+};
+
+export default SearchBar;
